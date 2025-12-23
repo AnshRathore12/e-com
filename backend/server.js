ff9323816d95c342b4548546478ecab7c8d69f57
AnshRathore12 <anshrathore30@gmail.com>
2025-12-23 13:19:10 +0530

    New Fix in Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,47 @@
-import express from "express"
-import dotenv from "dotenv"
-import path from "path"  // <--- Import 'path' module
-import authRoutes from "./routes/auth.route.js"
-import {connectDB} from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import productRoutes from "./routes/product.route.js"
-import cartRoutes from "./routes/cart.route.js"
-import couponRoutes from "./routes/coupon.route.js"
-import paymentRoutes from "./routes/payment.route.js"
-import analyticsRoutes from './routes/analytics.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path"; // 1. Import path
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const app=express();
-const PORT=process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Fix for ES Modules to get __dirname equivalent
+// 2. Define __dirname for ES Modules
 const __dirname = path.resolve();
 
-app.use(express.json({limit:"10mb"}));
-app.use(cookieParser())
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/products",productRoutes);
-app.use("/api/cart",cartRoutes);
-app.use("/api/coupons",couponRoutes);
-app.use("/api/payments",paymentRoutes);
-app.use("/api/analytics",analyticsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    });
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+// ------------------------------------
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
-})
-// lIC9tZG6ZZDqr3Tx
+});
